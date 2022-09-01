b2f3270809f57b6d52875257d814e28d7320497a
Mapc <mapctrigotixd@gmail.com>
2022-09-01 18:24:34 +0300

    +

--- general.py
+++ general.py
@@ -210,7 +210,7 @@ class GeneralMethods:
         status = "out_competition"
         notification = {
             "id_template": 41,
-            "comment": None,
+            "comment": "Поступающий не прошёл по конкурсу",
             "id_notices_types": 9
         }
         status_comment = None
